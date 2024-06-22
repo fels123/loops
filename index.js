2fb70fc2e0617a89da0a89c182ee6477f8ba7154
fels123 <kelengulienakhro@gmail.com>
2024-06-22 22:10:47 +0530

    all loops done

--- index.js
+++ index.js
@@ -41,27 +41,27 @@ switch (role) {
 // else console.log("Unknown user");
 
 // For Loop
-for (let i = 0; i < 5; i++) {
-  console.log("Hello World", i);
-}
+// for (let i = 0; i < 5; i++) {
+//   console.log("Hello World", i);
+// }
 
-// practical example odd numbers
-for (let i = 0; i < 5; i++) {
-  if (i % 2 !== 0) console.log(i);
-}
+// // practical example odd numbers
+// for (let i = 0; i < 5; i++) {
+//   if (i % 2 !== 0) console.log(i);
+// }
 
-//same example for odd numbers using while loop
-let i = 0;
-while (i <= 5) {
-  if (i % 2 !== 0) console.log(i);
-  i++;
-}
+// //same example for odd numbers using while loop
+// let i = 0;
+// while (i <= 5) {
+//   if (i % 2 !== 0) console.log(i);
+//   i++;
+// }
 
-//do-while loops executes once atleast even if the condition is false
-do {
-  if (i % 2 !== 0) console.log(i);
-  i++;
-} while (i <= 5);
+// //do-while loops executes once atleast even if the condition is false
+// do {
+//   if (i % 2 !== 0) console.log(i);
+//   i++;
+// } while (i <= 5);
 
 //infinite loop
 
@@ -69,3 +69,31 @@ do {
 //   console.log(i);
 //   i++;
 // }
+
+// for in loop
+const person = {
+  name: "Mosh",
+  age: 30,
+};
+for (let key in person) {
+  console.log(key, person[key]);
+}
+const colors = ["red", "green", "blue"];
+for (let index in colors) {
+  console.log(index, colors[index]);
+}
+
+//for of loop
+for (let color of colors) console.log(color);
+
+//break and continue
+let i = 0;
+while (i <= 10) {
+  if (i === 5) break;
+  // if (i % 2 === 0) {
+  //   i++;
+  //   continue; /////usage minimum
+  // }
+  console.log(i);
+  i++;
+}
